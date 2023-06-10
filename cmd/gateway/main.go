885381a99c1b9f0ff9d9148c71915ba06f645903
DavidDengHui <david.no.1@msn.cn>
2023-06-10 21:02:42 +0800

    Revert "update 1.01"
    This reverts commit 51d0ff53e5b30a3107ddb4a83bb9b143bfbe6aa6.

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -24,9 +24,6 @@ func main() {
 	http.Handle("/api/feed", feed2json.Handler(
 		feed2json.StaticURLInjector("https://news.ycombinator.com/rss"),
 		nil, nil, nil, cacheControlMiddleware))
-
-	http.Handle("/api/hello", "Hello, World! Go~")
-
 	log.Fatal(listener(portStr, nil))
 }
 
