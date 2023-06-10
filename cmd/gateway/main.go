781e07f4e23697a96c6ee0c79641350a656ef2ec
DavidDengHui <david.no.1@msn.cn>
2023-06-11 02:23:12 +0800

    update 1.20

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -10,11 +10,7 @@ import (
 func main() {
 	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
 	flag.Parse()
-	listener := gateway.ListenAndServe
-	portStr := ""
 	if *port != -1 {
-		portStr = fmt.Sprintf(":%d", *port)
-		listener = http.ListenAndServe
 		http.Handle("/", http.FileServer(http.Dir("./public")))
 	}
 
